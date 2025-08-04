451455686fbb2740319b24e2f98578835c1998e1
NadiiaUa23 <hasiminka92@gmail.com>
2025-08-04 19:04:37 +0200

    add  TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "modern-normalize";
 import "./global.css";
 import App from "./components/App/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
